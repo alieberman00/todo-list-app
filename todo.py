698ad08e3d4de5dce870405103ee47f901cc8c0d
alieberman00 <allison.lieberman@ascte.org>
2025-08-29 09:27:01 -0500

    Update todo.py

--- todo.py
+++ todo.py
@@ -8,14 +8,16 @@ def add_task(task):
     tasks.append(task)
 
 # Step 3: View tasks
-def view_tasks(task):
+def view_tasks():
     for i, task in enumerate(tasks, start=1):
         print(f"(i). {task}")
 # Step 4: Delete a task
-
+def delete_task(remove):
+    tasks.pop(remove)
 
 # Step 5: Mark task complete
-
+def mark_complete():
+    
 
 # Step 6: Save/load tasks (extra stretch for today)
 
@@ -25,6 +27,8 @@ if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
     view_tasks()
-    #mark_complete(0)
+    delete_task(0)
+    view_tasks()
+    mark_complete(0)
     #view_tasks()
     #save_tasks()
